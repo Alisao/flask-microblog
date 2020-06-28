f9d4ff831dfc2e1321eb3848ff1dc781420b9ca0
Alberto Isaboke <isabokea@gmail.com>
2020-06-28 11:36:59 +0300

    paginate posts on index and explore

--- app/routes.py
+++ app/routes.py
@@ -26,8 +26,10 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template("index.html", title='Home Page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template("index.html", title='Home Page', form=form, posts=posts.items)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -136,3 +138,12 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Explore', posts=posts.items)
